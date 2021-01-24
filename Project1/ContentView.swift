85d2c47a9f7186007846cdda8268c738d12ceafd
Artijan Zmajli <a1369234@univie.ac.at>
2021-01-24 22:16:17 +0100

    Initial Commit

---
+++ Project1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Project1
+//
+//  Created by Artijan Zmajli on 24.01.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Project1/Project1App.swift
@@ -0,0 +1,17 @@
+//
+//  Project1App.swift
+//  Project1
+//
+//  Created by Artijan Zmajli on 24.01.21.
+//
+
+import SwiftUI
+
+@main
+struct Project1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
